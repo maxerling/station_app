417e7aced98e2445da4215048df5b9febb4fcdaf
Max Erling <max.d.erling@gmail.com>
2022-03-09 22:41:38 +0100

    change colors

--- app/src/main/java/com/example/station_app/adapters/StationDepartureAdapter.kt
+++ app/src/main/java/com/example/station_app/adapters/StationDepartureAdapter.kt
@@ -1,10 +1,12 @@
 package com.example.station_app.adapters;
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.get
 import androidx.recyclerview.widget.RecyclerView
 import com.example.station_app.R
@@ -20,6 +22,7 @@ class StationDepartureAdapter: RecyclerView.Adapter<StationDepartureAdapter.Stat
         var departureTime : TextView = view.findViewById(R.id.departure_time)
         var destination : TextView = view.findViewById(R.id.destination)
         var trackNumber : TextView = view.findViewById(R.id.track_no)
+        val layout : ConstraintLayout = view.findViewById(R.id.station_departure)
 
     }
 
@@ -30,10 +33,16 @@ class StationDepartureAdapter: RecyclerView.Adapter<StationDepartureAdapter.Stat
     }
 
     override fun onBindViewHolder(holder: StationDepartureViewHolder, position: Int) {
+        if (position % 2 == 0)  {
+
+            holder.layout.setBackgroundColor(Color.parseColor("#EDEDED"))
+        } else {
+            holder.layout.setBackgroundColor(Color.parseColor("#FFFFFF"))
+        }
         val value = data[position]
-        holder.departureTime.text = "egg"
-        holder.destination.text = "sandwhich"
-        holder.trackNumber.text = "lol"
+        holder.departureTime.text = "departureTime"
+        holder.destination.text = "destination"
+        holder.trackNumber.text = "trackNumber"
        //holder.text.setOnClickListener {  }
 
 
