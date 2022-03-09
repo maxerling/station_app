c220ffae417f1f1c798ab1ac086eb416128f1e9c
Max Erling <max.d.erling@gmail.com>
2022-03-09 21:20:25 +0100

    setup station depature view

--- app/src/main/java/com/example/station_app/adapters/StationDepartureAdapter.kt
+++ app/src/main/java/com/example/station_app/adapters/StationDepartureAdapter.kt
@@ -3,7 +3,9 @@ package com.example.station_app.adapters;
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.view.get
 import androidx.recyclerview.widget.RecyclerView
 import com.example.station_app.R
 
@@ -15,20 +17,24 @@ class StationDepartureAdapter: RecyclerView.Adapter<StationDepartureAdapter.Stat
 
 
     class StationDepartureViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        var text : TextView = view.findViewById(R.id.text_item)
+        var departureTime : TextView = view.findViewById(R.id.departure_time)
+        var destination : TextView = view.findViewById(R.id.destination)
+        var trackNumber : TextView = view.findViewById(R.id.track_no)
 
     }
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StationDepartureViewHolder {
-        val layout : View = LayoutInflater.from(parent.context).inflate(R.layout.text_item_view,parent,false)
-        return StationDepartureViewHolder(layout)
+        val view : View = LayoutInflater.from(parent.context).inflate(R.layout.text_item_view,parent,false)
+        return StationDepartureViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: StationDepartureViewHolder, position: Int) {
         val value = data[position]
-        holder.text.text = value.toString()
-        holder.text.setOnClickListener {  }
+        holder.departureTime.text = "egg"
+        holder.destination.text = "sandwhich"
+        holder.trackNumber.text = "lol"
+       //holder.text.setOnClickListener {  }
 
 
 
